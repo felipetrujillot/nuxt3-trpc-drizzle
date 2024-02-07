3cba64472ffe57e800871b027f9aaaa15d9ab6e1
Felipe Trujillo <felipe.trujillo@valorunico.cl>
2024-02-07 15:12:42 -0300

    e

---
+++ components/Product.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+
+console.log("hererere");
+</script>
+
+<template>
+  <Card>
+    <CardContent>
+      <p class="text-sm">
+        {{ name }}
+      </p>
+    </CardContent>
+  </Card>
+</template>
